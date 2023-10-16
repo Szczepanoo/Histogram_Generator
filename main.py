8a47df196394efba38d7ac56e080c58044e4e514
Jakub <jakub.szczepanski02@gmail.com>
2023-10-16 12:00:42 +0200

    Dodano funkcję save_histogram_to_file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib as plt
 def generate_letter_histogram(file_path):
     letter_counts = {}
 
@@ -16,3 +17,21 @@ def generate_letter_histogram(file_path):
         return None
 
     return letter_counts
+
+
+def save_histogram_to_file(letter_counts, output_file):
+    if letter_counts is not None:
+        letters = list(letter_counts.keys())
+        counts = list(letter_counts.values())
+
+        plt.bar(letters, counts)
+        plt.xlabel('Litery')
+        plt.ylabel('Liczba wystąpień')
+        plt.title('Histogram częstotliwości liter')
+
+        # Ustaw etykiety na osi X
+        plt.xticks(letters)
+
+        # Zapisz histogram do pliku PNG lub JPG
+        plt.savefig(output_file, format='png')  # Zmień 'png' na 'jpg' jeśli chcesz format JPG
+        plt.show()
