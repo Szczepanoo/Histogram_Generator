f49eaaa8ba06638c47b7517a31bb9c4861a99bc4
Jakub <jakub.szczepanski02@gmail.com>
2023-10-17 23:44:34 +0200

    In develop

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import urllib.request
 import matplotlib.pyplot as plt
-def generate_letter_histogram(file_path):
-    letter_counts = {}
+import os
+
+def read_text_from_file(file_path):
     try:
         with open(file_path, 'r') as file:
             text = file.read()
-            for char in text:
-                if char.isalpha():
-                    char = char.lower()
-                    if char in letter_counts:
-                        letter_counts[char] += 1
-                    else:
-                        letter_counts[char] = 1
     except FileNotFoundError:
         print("Nie można znaleźć pliku:", file_path)
         return None
 
+    return text
+
+
+def generate_histogram_from_text(text):
+    letter_counts = {}
+    for char in text:
+        if char.isalpha():
+            # char = char.lower()  #wyłączenie wielkich liter
+            if char in letter_counts:
+                letter_counts[char] += 1
+            else:
+                letter_counts[char] = 1
     return letter_counts
 
 
@@ -28,18 +35,62 @@ def save_histogram_to_file(letter_counts, output_file):
         plt.ylabel('Liczba wystąpień')
         plt.title('Histogram częstotliwości liter')
 
-
         plt.xticks(letters)
-
 
         plt.savefig(output_file, format='png')
         plt.show()
 
+        print("Zapisano histogram do pliku histogram.png")
 
-file_path = 'C:\\jakub.szczepanski\\Semestr III\\Specjalistyczne Oprogramowanie Narzędziowe\\Ćwiczenia\\SON_Projekt1\\source_file.txt'
 
-output_file = 'C:\\jakub.szczepanski\\Semestr III\\Specjalistyczne Oprogramowanie Narzędziowe\\Ćwiczenia\\SON_Projekt1\\histogram.png'
-letter_counts = generate_letter_histogram(file_path)
-if letter_counts is not None:
-    save_histogram_to_file(letter_counts, output_file)
-    print("Zapisano histogram do pliku histogram.png")
+def read_text_from_url(url):
+    try:
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        text = data.decode("utf-8")
+        return text
+    except Exception as e:
+        print("Błąd podczas pobierania tekstu", e)
+        return ""
+
+
+def generate_and_save(text, output_file):
+    save_histogram_to_file(generate_histogram_from_text(text), output_file)
+
+
+sciezka = os.getcwd()
+file_path = sciezka + ('\\source_file.txt')
+output_file = sciezka + ('\\histogram.png')
+ans = input("Domyślnie liczane są wszystkie litery w tekście. Czy chcesz podać listę liter do sprawdzenia?")
+
+if ans.lower() == "tak" or ans.lower() == "t":
+    litery = input("Podaj zestaw liter, oddziel poszczególne litery znakiem ','. Przykład: a,b,c : ")
+    for char in litery.split(','):
+        print(char)
+
+
+print("Wybierz skąd wprowadzić dane:")
+print("1. Wprowadź z klawiatury.")
+print("2. Podaj adres URL.")
+print("3. Wczytaj z pliku source.txt")
+option = input("Wybierz (1-2):")
+text = ""
+flag = True
+while flag:
+    if option == "1":
+        flag = False
+        text = input("Wprowadź tekst:")
+        generate_and_save(text, output_file)
+
+    elif option == "2":
+        flag = False
+        url = input("Wprowadź adres: ")
+        text = read_text_from_url(url)
+        generate_and_save(text, output_file)
+
+    elif option == "3":
+        flag = False
+        text = read_text_from_file(file_path)
+        generate_and_save(text,output_file)
+    else:
+        option = input("Błąd. Wybierz (1-2):")
