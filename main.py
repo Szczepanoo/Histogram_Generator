f6b66be881a7c39ce29b71264cb68a1a64d48671
Jakub <jakub.szczepanski02@gmail.com>
2023-10-17 21:32:35 +0200

    Ukończono menu-feature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import urllib.request
 import matplotlib.pyplot as plt
 import os
+
+
 def read_text_from_file(file_path):
     try:
         with open(file_path, 'r') as file:
@@ -23,6 +25,7 @@ def generate_histogram_from_text(text):
                 letter_counts[char] = 1
     return letter_counts
 
+
 def save_histogram_to_file(letter_counts, output_file):
     if letter_counts is not None:
         letters = list(letter_counts.keys())
@@ -33,14 +36,13 @@ def save_histogram_to_file(letter_counts, output_file):
         plt.ylabel('Liczba wystąpień')
         plt.title('Histogram częstotliwości liter')
 
-
         plt.xticks(letters)
-
 
         plt.savefig(output_file, format='png')
         plt.show()
 
         print("Zapisano histogram do pliku histogram.png")
+
 
 def read_text_from_url(url):
     try:
@@ -52,14 +54,14 @@ def read_text_from_url(url):
         print("Błąd podczas pobierania tekstu", e)
         return ""
 
-def generate_and_save(text,output_file):
-    save_histogram_to_file(generate_histogram_from_text(text),output_file)
 
+def generate_and_save(text, output_file):
+    save_histogram_to_file(generate_histogram_from_text(text), output_file)
 
 
 sciezka = os.getcwd()
-file_path = sciezka+('\\source_file.txt')
-output_file = sciezka+('\\histogram.png')
+file_path = sciezka + ('\\source_file.txt')
+output_file = sciezka + ('\\histogram.png')
 print("Wybierz skąd wprowadzić dane:")
 print("1. Wprowadź z klawiatury.")
 print("2. Podaj adres URL.")
@@ -70,15 +72,13 @@ while flag:
     if option == "1":
         flag = False
         text = input("Wprowadź tekst:")
-
-        generate_and_save(text,output_file)
+        generate_and_save(text, output_file)
 
     elif option == "2":
         flag = False
         url = input("Wprowadź adres: ")
         text = read_text_from_url(url)
-
-        generate_and_save(text,output_file)
+        generate_and_save(text, output_file)
 
     else:
         option = input("Błąd. Wybierz (1-2):")
