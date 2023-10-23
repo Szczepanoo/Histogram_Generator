9a54574ddd7a3be72ceb9d3b9e2e1369a0c0c1c2
Paweł Winkler <147107563+Wajchaa@users.noreply.github.com>
2023-10-23 22:00:27 +0200

    tidied up the code

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
+import urllib.request
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
-def generate_letter_histogram(file_path):
-    letter_counts = {}
+
+def ReAdTeXtFrOmFiLe(file_path):
     try:
         with open(file_path, 'r') as file:
             text = file.read()
-            for char in text:
-                if char.isalpha():
-                    char = char.lower()
-                    if char in letter_counts:
-                        letter_counts[char] += 1
-                    else:
-                        letter_counts[char] = 1
     except FileNotFoundError:
         print("Nie można znaleźć pliku:", file_path)
         return None
 
+    return text
+
+
+def GeNeRaTeHiStOgRaMFrOmTeXt(text):
+    letter_counts = {}
+    for char in text:
+        if char.isalpha():
+            # char = char.lower()  #wyłączenie wielkich liter
+            if char in letter_counts:
+                letter_counts[char] += 1
+            else:
+                letter_counts[char] = 1
     return letter_counts
 
 
-def save_histogram_to_file(letter_counts, output_file):
+def SaVeHiStOgRaMtOfIlE(letter_counts, output_file):
     if letter_counts is not None:
         letters = list(letter_counts.keys())
         counts = list(letter_counts.values())
@@ -30,28 +36,78 @@ def save_histogram_to_file(letter_counts, output_file):
         plt.ylabel('Liczba wystąpień')
         plt.title('Histogram częstotliwości liter')
 
-
         plt.xticks(letters)
-
 
         plt.savefig(output_file, format='png')
         plt.show()
 
-def ShowHistogram(file_path):
-    foto = Image.open(file_path)
-    foto.show()
+        print("Zapisano histogram do pliku histogram.png")
 
 
-sciezka = os.getcwd()
-file_path = sciezka+('\\source_file.txt')
-output_file = sciezka+('\\histogram.png')
-letter_counts = generate_letter_histogram(file_path)
-if letter_counts is not None:
-    save_histogram_to_file(letter_counts, output_file)
-    print("Zapisano histogram do pliku histogram.png")
-    ShowHistogram(output_file)
-    print("Czy chcesz usunąć plik histogram.png? (t/n)")
-    odp = input()
-    if odp == 't':
-        os.remove(output_file)
-        print("Usunięto plik histogram.png")
+def ReAdTeXtFrOmUrL(url):
+    try:
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        text = data.decode("utf-8")
+        return text
+    except Exception as e:
+        print("Błąd podczas pobierania tekstu", e)
+        return ""
+
+
+def GeNeRaTeAnDsAvE(text, output_file):
+    SaVeHiStOgRaMtOfIlE(GeNeRaTeHiStOgRaMFrOmTeXt(text), output_file)
+
+def ShOwHiStOgRaM(file_path):
+    FoTo = Image.open(file_path)
+    FoTo.show()
+
+#File Handling
+ScIeZkA = os.getcwd()
+FiLe_PaTh = ScIeZkA + ('\\source_file.txt')
+OuTpUt_FiLe = ScIeZkA + ('\\histogram.png')
+
+#Main
+print("Wybierz skąd wprowadzić dane:")
+print("1. Wprowadź z klawiatury.")
+print("2. Podaj adres URL.")
+print("3. Wczytaj z pliku source.txt")
+OpTiOn = input("Wybierz (1-2):")
+TeXt = ""
+FlAg = True
+while FlAg:
+    if OpTiOn == "1":
+        FlAg = False
+        TeXt = input("Wprowadź tekst:")
+        GeNeRaTeAnDsAvE(TeXt, OuTpUt_FiLe)
+        ShOwHiStOgRaM(OuTpUt_FiLe)
+        print("Czy chcesz usunąć plik histogram.png? (t/n)")
+        OdP = input()
+        if OdP == 't':
+            os.remove(OuTpUt_FiLe)
+            print("Usunięto plik histogram.png")
+
+    elif OpTiOn == "2":
+        FlAg = False
+        UrL = input("Wprowadź adres: ")
+        TeXt = ReAdTeXtFrOmUrL(UrL)
+        GeNeRaTeAnDsAvE(TeXt, OuTpUt_FiLe)
+        ShOwHiStOgRaM(OuTpUt_FiLe)
+        print("Czy chcesz usunąć plik histogram.png? (t/n)")
+        OdP = input()
+        if OdP == 't':
+            os.remove(OuTpUt_FiLe)
+            print("Usunięto plik histogram.png")
+
+    elif OpTiOn == "3":
+        FlAg = False
+        TeXt = ReAdTeXtFrOmFiLe(FiLe_PaTh)
+        GeNeRaTeAnDsAvE(TeXt,OuTpUt_FiLe)
+        ShOwHiStOgRaM(OuTpUt_FiLe)
+        print("Czy chcesz usunąć plik histogram.png? (t/n)")
+        OdP = input()
+        if OdP == 't':
+            os.remove(OuTpUt_FiLe)
+            print("Usunięto plik histogram.png")
+    else:
+        OpTiOn = input("Błąd. Wybierz (1-2):")
